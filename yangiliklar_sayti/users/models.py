606b5d4e8e10672e8bbf8d6b4bfb231029e464d7
gaybullayevblog <gaybullayevblog@gmail.com>
2023-08-14 17:23:44 +0500

    done

--- yangiliklar_sayti/users/models.py
+++ yangiliklar_sayti/users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
-class CustomUser(AbstractBaseUser):
-    
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    address = models.CharField(max_length=200)
+    job = models.CharField(max_length=50)
